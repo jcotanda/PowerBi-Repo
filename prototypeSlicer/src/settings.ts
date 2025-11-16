ca28808c134d2e6a32153e96ca69c0bd6a101bf5
Javier <jacollo98@gmail.com>
2025-11-16 21:17:23 +0100

    Add files via upload

---
+++ prototypeSlicer/src/settings.ts
@@ -0,0 +1,46 @@
+"use strict";
+
+import { formattingSettings } from "powerbi-visuals-utils-formattingmodel";
+
+import FormattingSettingsCard = formattingSettings.SimpleCard;
+import FormattingSettingsSlice = formattingSettings.Slice;
+import FormattingSettingsModel = formattingSettings.Model;
+
+/**
+ * Data Point Formatting Card
+ */
+class DataPointCardSettings extends FormattingSettingsCard {
+
+    fill = new formattingSettings.ColorPicker({
+        name: "fill",
+        displayName: "Fill",
+        value: { value: "" }
+    });
+
+    fillRule = new formattingSettings.ColorPicker({
+        name: "fillRule",
+        displayName: "Color saturation",
+        value: { value: "" }
+    });
+
+    fontSize = new formattingSettings.NumUpDown({
+        name: "fontSize",
+        displayName: "Text Size",
+        value: 12
+    });
+
+    name: string = "dataPoint";
+    displayName: string = "Data colors";
+    slices: Array<FormattingSettingsSlice> = [this.fill, this.fillRule, this.fontSize];
+}
+
+/**
+* visual settings model class
+*
+*/
+export class VisualFormattingSettingsModel extends FormattingSettingsModel {
+    // Create formatting settings model formatting cards
+    dataPointCard = new DataPointCardSettings();
+
+    cards = [this.dataPointCard];
+}
